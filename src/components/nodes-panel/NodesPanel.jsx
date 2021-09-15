4412c911aca9eee5812cb15ab7e8af2597b505bd
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2021-09-15 10:58:01 -0500

    added title to NodesPanel and another node
    title and line in white and centered
    
    cambio color fondo y links
    
    links con color correcto

--- src/components/nodes-panel/NodesPanel.jsx
+++ src/components/nodes-panel/NodesPanel.jsx
@@ -6,8 +6,20 @@ class NodesPanel extends React.Component {
         return (
             <div className="panel-wrapper">
                 <div className="nodes-panel">
+                    <div className='nodes-panel-title'>
+                        <h1>Hello World!</h1>
+                    </div>
+                    <hr
+                        style={{
+                            height: 0
+                        }}
+                    />
+                    <br/>
                     <div className='node-wrapper'>
-                        <Node type='imagenode' color='rgb(238, 238, 238)'/>
+                        <Node type='imagenode' color='rgb(238, 238, 238)' />
+                    </div>
+                    <div className='node-wrapper'>
+                        <Node type='imagenode' color='rgb(238, 238, 238)' />
                     </div>
                     {/* Other nodes */}
                 </div>
